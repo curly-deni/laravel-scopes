5793cf14e6566e213b6fcf0b06ae42d771961a09
Danila Mikhalev <danila.mihalev@yandex.ru>
2025-04-26 18:41:54 +0300

    add package to about

--- src/LaravelScopesServiceProvider.php
+++ src/LaravelScopesServiceProvider.php
@@ -2,11 +2,26 @@
 
 namespace Aesis\Scopes\LaravelScopes;
 
+use Composer\InstalledVersions;
+use Illuminate\Foundation\Console\AboutCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LaravelScopesServiceProvider extends PackageServiceProvider
 {
+
+    protected function registerAbout(): void
+    {
+        if (! class_exists(InstalledVersions::class) || ! class_exists(AboutCommand::class)) {
+            return;
+        }
+
+        AboutCommand::add('Laravel Scopes', static fn () => [
+            'Author' => 'Danila Mikhalev <danila@dan-mi.ru>',
+            'Version' => InstalledVersions::getPrettyVersion('curly-deni/laravel-scopes'),
+        ]);
+    }
+
     public function configurePackage(Package $package): void
     {
         $package
